5264c89137a29bb4d2210b68208e10517dc86fd6
Navin grg <grgnabin60@gmail.com>
2024-05-23 00:30:39 +0545

    Utils code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDb.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000,() => {
+        console.log(`Server listening on port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => console.log("Mongo connection Error: ", err))
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(
+        data,
+        statusCode,
+        message = "Sucess"
+    ){
+        this.data = data
+        this.message = message
+        this.statusCode = statusCode
+        this.success = statusCode < 400
+    }
+}
