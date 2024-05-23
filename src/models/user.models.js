90fed8b6193640597588b6e35be814167a33c118
Navin grg <grgnabin60@gmail.com>
2024-05-24 00:15:21 +0545

    uploaded file handling through cloudinary and multer

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -48,8 +48,12 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function(next) {
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
-    next();
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (err) {
+        next(err);
+    }
 })
 
 userSchema.methods.isPasswordCorrect =async function(password) {
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCLoudinary = async (filepath) => {
+    try {
+        if(!filepath) return null;
+        //upload the file to cloudinary
+        const response = await cloudinary.uploader.upload(filepath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded sucessfully 
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filepath)  //remove the locally saced temporary file as the opretaion got failed
+        return null
+}
+}
+
+export { uploadOnCLoudinary }
