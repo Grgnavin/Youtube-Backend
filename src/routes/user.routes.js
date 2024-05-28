eeff1086b72dd9d3afc6de20180d499a5fcc7595
Navin grg <grgnabin60@gmail.com>
2024-05-29 00:48:45 +0545

    added new routes for userController

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { 
+        loginUser,
+        logoutUser,
+        registerUser,
+        refreshAccessToken,
+        changePassword,
+        getUser,
+        updateAccountDetails, 
+        updateAvatarFile, 
+        updateCoverImage, 
+        getUserChannelProfile, 
+        getWatchHistory 
+    } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -21,6 +33,19 @@ router.route('/register').post(
 router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT ,logoutUser)
 router.route('/refresh-Token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT ,changePassword)
+router.route('/current-user').post(verifyJWT ,getUser)
+router.route('/update-account').patch(verifyJWT ,updateAccountDetails)
+
+router.route('/Avatar').post(verifyJWT , upload.single("avatar"), updateAvatarFile)
+router.route('/CoverImage').post(verifyJWT , upload.single("coverImage"), updateCoverImage)
+
+router.route('/channel/:username').post(verifyJWT, getUserChannelProfile)
+router.route('/history').get(verifyJWT, getWatchHistory)
+
+
+
+
 
 
 export default router;
