436fe1c61da4b00e3ffe4fb08e9426c0a92ea15a
Navin grg <grgnabin60@gmail.com>
2024-05-29 12:49:36 +0545

    Added the assignment

--- src/app.js
+++ src/app.js
@@ -16,14 +16,26 @@ app.use(cookieParser());
 
 //routes
 import userRoute from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
-app.use("/api/v1/users", userRoute);
-app.get("/api/v1/", (req,res) => {
-    res.status(201).json({
-        message: "Welcome to the Youtube backend"
-    })
-});
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRoute)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
